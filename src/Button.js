c437ff54ab226b39ec0c833479f1d10161f384e1
diptigandhi24 <gandhidipti.s@gmail.com>
2019-09-16 13:43:44 +0530

    Made button component more flexible and less repitative

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,5 @@
 import React from "react";
 import "./Button.css";
-export default function Button() {
-  return (
-    <div>
-      <button className={"btn btn-primary"}>Primary Button</button>
-      <button className={"btn btn-secondary"}>Secondary Button</button>
-      <button className={"btn btn-success"}>Success Button</button>
-      <button className={"btn btn-warning"}>Warning Button</button>
-    </div>
-  );
+export default function Button(props) {
+  return <button className={`btn btn-${props.btnType}`}>{props.name}</button>;
 }
